587d6c66d5acb07c7db114b6889c44c9c6663198
Sergei Belokon <srj.belokon@gmail.com>
2021-01-20 13:33:26 +0300

    OBS-01 Tested closures a little

--- src/greet.rs
+++ src/greet.rs
@@ -1,9 +1,12 @@
 use std::env;
 
 pub fn say_hello() {
+    // The func parses command line arguments takes 2nd
+    // (coz 1st it is a function name itself) and prints greetings
+    // if there no arguments the func panics
     let name = env::args().skip(1).next();
     match name {
         Some(n) => println!("Hello there. {}", n),
-        None => panic!("Didn't recieve any name"),
+        None => println!("Didn't recieve any name"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod greet;
 
 fn main() {
-    greet::say_hello()
+    greet::say_hello();
+
+    //testing closures
+    let double_it = |x: u32| x * 2;
+    let value = 5;
+    let twice = double_it(value);
+    println!("{} doubled is {}", value, twice);
+
+    let multiple_value = |b: u32, c: u32| {
+        let z = b + c;
+        z * twice
+    };
+    let execute_closure = multiple_value(4, 7);
+    println!("Result from closure: {}", execute_closure);
 }
