3096add958deb2a7d94a6e301fac2b003591b34f
Sergei Belokon <srj.belokon@gmail.com>
2021-03-20 13:48:54 +0300

    OBS-01 Checking command line parsing with the `clap` crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-pub mod greet;
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
 
 fn main() {
-    let mut user = greet::User::generate("Rinzler", "male", "white", 67);
-    println!("{:?}", user);
+    let cfg_name = ObsArgs::new();
+    println!("{}", cfg_name.cfg);
+}
 
-    user.set_age(113);
-    println!("{:?}", user);
+struct ObsArgs {
+    cfg: String,
+}
+
+impl ObsArgs {
+    fn new() -> Self {
+        let param_cfg = Arg::with_name("cfg")
+            .short("t")
+            .long("rotate")
+            .takes_value(true)
+            .multiple(true)
+            .value_name("FILE(S)")
+            .help("Rotate specified list of files");
+
+        let matches = App::new("obs")
+            .version("v1.0-beta")
+            .about("Rust implementation of the dotfile rotation util")
+            .author(crate_authors!("\n"))
+            .args(&[param_cfg])
+            .get_matches();
+
+        let name = matches.value_of("cfg").unwrap();
+
+        ObsArgs {
+            cfg: String::from(name),
+        }
+    }
 }
