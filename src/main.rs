b00282f32e180f797548a8cc1001e90e0abd42de
Sergei Belokon <srj.belokon@gmail.com>
2021-03-20 13:55:33 +0300

    Feature/obs 01 a tour of the language (#1)
    * OBS-01 Exploring a Chapter1 of the book..
    
    Used:
    module import with the 'mod';
    'args' module from 'env' crate within the methods 'skip' and 'next';
    match statement with Some and None;
    Also panic! and println! macros;
    variable binding;
    'pub' keyword and function creating.
    Maybe forgot something, but, hope, it'll be covered in future topics
    
    * OBS-01 Tested closures a little
    
    * OBS-01 Looked into the structs, commented closures section
    
    * OBS-01 playing with associated and instance's methods
    
    * OBS-01 added some comments for memorization
    
    * OBS-01 Checking command line parsing with the `clap` crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let cfg_name = ObsArgs::new();
+    println!("{}", cfg_name.cfg);
+}
+
+struct ObsArgs {
+    cfg: String,
+}
+
+impl ObsArgs {
+    fn new() -> Self {
+        let param_cfg = Arg::with_name("cfg")
+            .short("t")
+            .long("rotate")
+            .takes_value(true)
+            .multiple(true)
+            .value_name("FILE(S)")
+            .help("Rotate specified list of files");
+
+        let matches = App::new("obs")
+            .version("v1.0-beta")
+            .about("Rust implementation of the dotfile rotation util")
+            .author(crate_authors!("\n"))
+            .args(&[param_cfg])
+            .get_matches();
+
+        let name = matches.value_of("cfg").unwrap();
+
+        ObsArgs {
+            cfg: String::from(name),
+        }
+    }
 }
